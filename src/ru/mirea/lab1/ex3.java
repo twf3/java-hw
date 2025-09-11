a123d4702a171c56734e09b5c6dfe7c25bbaed4f
ksenia <furbyksu@gmail.com>
2025-09-11 22:45:47 +0300

    1 commit

---
+++ src/ru/mirea/lab1/ex3.java
@@ -0,0 +1,14 @@
+package ru.mirea.lab1;
+
+public class ex3 {
+    public static void main(String[] args) {
+        for (String str : args) {
+            System.out.println("аргумент = " + str);
+        }
+    }
+}
+
+
+// C:\Users\kseni>cd C:\Users\kseni\IdeaProjects\untitled\src
+// C:\Users\kseni\IdeaProjects\untitled\src>javac ru\mirea\lab1\ex3.java
+// C:\Users\kseni\IdeaProjects\untitled\src>java ru.mirea.lab1.ex3 значени1 значение2
