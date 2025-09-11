83d5c96ee7e3e483f65d9b6f3d8e83fbe174473c
ksenia <furbyksu@gmail.com>
2025-09-12 00:02:48 +0300

    4 номер

---
+++ src/ru/mirea/lab1/ex4.java
@@ -0,0 +1,10 @@
+package ru.mirea.lab1;
+
+public class ex4 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        for (int i=2; i<10; i++) {
+            System.out.println("1/" + i);
+        }
+    }
+}
