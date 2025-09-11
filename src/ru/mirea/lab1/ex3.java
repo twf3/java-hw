7d01f4a9254db68b7266d38f2806945156fd8eff
ksenia <furbyksu@gmail.com>
2025-09-11 23:42:29 +0300

    исправлена ошибка в комментарии

--- src/ru/mirea/lab1/ex3.java
+++ src/ru/mirea/lab1/ex3.java
@@ -3,11 +3,11 @@ package ru.mirea.lab1;
 public class ex3 {
     public static void main(String[] args) {
         for (String str : args) {
-            System.out.println("аргумент = " + str);
+            System.out.println("х = " + str);
         }
     }
 }
 
-/*C:\Users\kseni>cd C:\Users\kseni\IdeaProjects\untitled\src
-C:\Users\kseni\IdeaProjects\untitled\src>javac ru\mirea\lab1\ex3.java
-C:\Users\kseni\IdeaProjects\untitled\src>java ru.mirea.lab1.ex3 значени1 значение2 */
+/*cd C:\Users\kseni\IdeaProjects\ untitled\src
+javac ru\mirea\lab1\ex3.java
+java ru.mirea.lab1.ex3 значениe1 значение2 */
