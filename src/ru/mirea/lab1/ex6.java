9a8f95f52046a36485577285eb7083eb4ceab048
ksenia <furbyksu@gmail.com>
2025-09-13 17:13:55 +0300

    все переименовано

--- src/ru/mirea/lab1/ex6.java
+++ src/ru/mirea/lab1/ex6.java
@@ -2,9 +2,11 @@ package ru.mirea.lab1;
 
 public class ex6 {
     public static void main(String[] args) {
-        System.out.println(1);
-        for (int i=2; i<10; i++) {
-            System.out.println("1/" + i);
+        float summ = 0.0f;
+        System.out.println("Первые 10 чисел гармонического ряда:");
+        for (int i=1; i<11; i++) {
+            summ += 1.0f / i;
+            System.out.printf("S(%d) = %.3f%n", i, summ);
         }
     }
 }
