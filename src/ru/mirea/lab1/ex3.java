2a775274660f4e69a59625648adedb3261bf6ff5
ksenia <furbyksu@gmail.com>
2025-09-14 12:01:30 +0300

    изменена структура

--- src/ru/mirea/lab1/ex3.java
+++ src/ru/mirea/lab1/ex3.java
@@ -9,7 +9,8 @@ public class ex3 {
         int[] arr = new int[a];
         for (int i=0; i<arr.length; i++) {
             arr[i] = sc.nextInt();
-            summ = summ + arr[i];
+            summ += arr[i];
         }
+        System.out.println(summ);
         System.out.println(summ / arr.length);
     }}
